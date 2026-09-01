a3227d98d3dcd6295edb3c5bb64d586af7640bf6
12pimpi <12hangyulnoob@gmail.com>
2026-09-02 07:19:14 +0900

    problem2.cpp 업데이트

--- problem2.cpp
+++ problem2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -6,5 +7,6 @@ int main(){
     double a,b;
     cin >> a >> b;
     double result = ((a * 2 ) / (1 + (a / b)));
+    result = round(result * 100) / 100
     cout << result << endl;
 }
