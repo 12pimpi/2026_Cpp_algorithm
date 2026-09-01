8c0eac7d17a581dab48020cf4b4f0aa1e880433e
CuteAbomination <12hangyulnoob@gmail.com>
2026-09-02 07:24:12 +0900

    main.exe가 problem2.cpp로 컴파일됨. 실행결과 살짝 아쉬움. 소수점 2자리까지 개선요함

--- problem2.cpp
+++ problem2.cpp
@@ -7,6 +7,6 @@ int main(){
     double a,b;
     cin >> a >> b;
     double result = ((a * 2 ) / (1 + (a / b)));
-    result = round(result * 100) / 100
+    result = round(result * 100) / 100;
     cout << result << endl;
 }
