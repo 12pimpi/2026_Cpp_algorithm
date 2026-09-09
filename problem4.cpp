28e229e2a0e97f02657458f777559a12ca9f848e
CuteAbomination <12hangyulnoob@gmail.com>
2026-09-09 20:09:33 +0900

    최종본. 버블sort추가, count가 1일경우 추가

--- problem4.cpp
+++ problem4.cpp
@@ -2,15 +2,9 @@
 
 using namespace std;
 
-struct time_plan{
-    int start_time;
-    int end_time;
-};
-
 int main(){
     int count;
-    int second_index_value = 0;
-    int result = 0;
+    int result = 1;
     cin >> count;
 
     int (*input)[2] = new int[count][2];
@@ -18,16 +12,30 @@ int main(){
         cin >> input[i][0] >> input[i][1];
     }
     for(int i = 0;i < count; i++){
+        for(int j = 0;j < count-i-1;j++){
+            if(input[j][1] > input[j+1][1]){
+                int temp = input[j][1];
+                input[j][1] = input[j+1][1];
+                input[j+1][1] = temp;
+
+                temp = input[j][0];
+                input[j][0] = input[j+1][0];
+                input[j+1][0] = temp;
+            }
+        }
+    }
+
+    for(int i = 0;i < count; i++){
         int sec_index_value = input[i][1];
         int temp_result = 1;
         for(int j = 0;j < count; j++){
             if(sec_index_value <= input[j][0]){
                 temp_result++;
-                sec_index_value = input[j][1];
-                j = -1;
                 if(temp_result > result){
                     result = temp_result;
                 }
+                sec_index_value = input[j][1];
+                j = -1;
             } else {
                 continue;
             }
